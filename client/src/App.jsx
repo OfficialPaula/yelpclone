73986ad9107f899793ae3d393e98160dd518e8aa
OfficialPaula <95579427+OfficialPaula@users.noreply.github.com>
2023-05-28 23:45:40 -0400

    Enhance home page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import UpdatePage from './routes/UpdatePage';
-import RestaurantDetailPage from './routes/RestaurantDetailPage.jsx';
+import RestaurantDetailPage from './routes/RestaurantDetailPage';
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/restaurants/:id/update" element={<UpdatePage />} />
-          <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
-        </Routes>
-      </Router>
+    <div className="container">
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/restaurants/:id/update" element={<UpdatePage />} />
+            <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 };
